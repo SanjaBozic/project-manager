8fe383eb9be6f4269b3a8a7a8bd888256bc2dece
my.life.my.music.my.dream@gmail.com <my.life.my.music.my.dream@gmail.com>
2025-07-30 23:58:38 +0200

    dark mode and main nav

---
+++ src/components/MainNavigation.vue
@@ -0,0 +1,42 @@
+
+<template>
+    <div class="card">
+        <Menubar :model="items" />
+        <ToggleSwitch v-model="checked" @click="toggleDarkMode()">
+        <template #handle="{ checked }">
+            <i :class="['!text-xs pi', { 'pi-moon': checked, 'pi-sun': !checked }]" />
+        </template>
+      </ToggleSwitch>
+    </div>
+</template>
+
+<script setup>
+import { Menubar } from "primevue";
+import { ref } from "vue";
+import "@/assets/main-navigation.css"
+import { ToggleSwitch } from "primevue";
+
+let checked = ref(false);
+const toggleDarkMode = () =>  {
+  document.documentElement.classList.toggle('dark-mode');
+}
+
+const items = ref([
+    {
+        label: 'Dashboard',
+        icon: 'pi pi-chart-bar'
+    },
+    {
+        label: 'Work Items',
+        icon: 'pi pi-objects-column'
+    },
+    {
+        label: 'Backlogs',
+        icon: 'pi pi-th-large',
+    },
+    {
+        label: 'Sprints',
+        icon: 'pi pi-stop'
+    }
+]);
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import 'primeicons/primeicons.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: '.dark-mode',
+            cssLayer: {
+                name: 'primevue',
+                order: 'primevue'
+            }
+        }
+    }
+});
+app.mount("#app");
